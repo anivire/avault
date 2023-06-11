3908e05f3e7f18dd4d133017b1c61d6a913262ee
anivire <anivire.work@gmail.com>
2023-06-11 13:59:12 +0300

    🐛 fix shedule request

--- server/api/v1/shedule/index.get.ts
+++ server/api/v1/shedule/index.get.ts
@@ -4,7 +4,7 @@ export default defineEventHandler(async (event) => {
     const query = getQuery(event);
 
     const miscClient = new Jakan.Jakan().withMemory().forMisc();
-    const sheduleData = await miscClient.schedules({kids: 'false', limit: 100, filter: query.day as "monday" | "tuesday" | "wednesday" | "thursday" | "friday" | "unknow" | "other" | undefined});
+    const sheduleData = await miscClient.schedules({kids: 'false', limit: 25, filter: query.day as "monday" | "tuesday" | "wednesday" | "thursday" | "friday" | "unknow" | "other" | undefined});
 
     return sheduleData;
 })
