5d49c91a27782bbf4a569d23375aa61483e75b75
Nikita Grichan <anivire.work@gmail.com>
2024-02-04 21:10:14 +0300

    state update

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,6 +1,10 @@
 <template>
     <ToastWrapper/>
     <NuxtLoadingIndicator />
+    <div class="bg-rose-600 flex flex-row gap-2 p-3 items-center justify-center">
+        <Icon name="ri:info-i" class="text-base"/>
+        <p class="text-sm font-bold">AVAULT support has been temporarily suspended due to a complete reorganization and update of the project. Some functions are currently unavailable, but all your data will be saved in its current form as soon as the project is updated.</p>
+    </div>
     <div class="flex flex-col relative min-h-screen">
         <div class="absolute z-10 w-full"><AppHeader/></div> 
         <div class="mb-28 grow"><NuxtPage/></div>
